d9a4a8cdd07938e67fd79a4b95b5523a4169184e
andresdlt03 <andelatmo@gmail.com>
2024-03-26 18:55:26 +0100

    set smart trade theme

--- app/src/main/java/com/example/smarttrade/MainActivity.kt
+++ app/src/main/java/com/example/smarttrade/MainActivity.kt
@@ -3,8 +3,9 @@ package com.example.smarttrade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,10 +20,9 @@ class MainActivity : ComponentActivity() {
             SmartTradeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    Greeting("hola")
                 }
             }
         }
@@ -31,10 +31,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Click me")
+        }
+    }
 }
 
 @Preview(showBackground = true)
