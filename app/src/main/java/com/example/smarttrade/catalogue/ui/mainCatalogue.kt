ad357f532b19017a85b0c632f3982d344cf0cc57
Juan Pablo Cortés Hernández <pablocortes911@gmail.com>
2024-04-15 22:24:03 +0200

    Empezando ver productos

--- app/src/main/java/com/example/smarttrade/catalogue/ui/mainCatalogue.kt
+++ app/src/main/java/com/example/smarttrade/catalogue/ui/mainCatalogue.kt
@@ -1,15 +1,19 @@
 package com.example.smarttrade.catalogue.ui
 
 import android.widget.ToggleButton
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
@@ -17,6 +21,7 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
@@ -27,8 +32,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 
 @Composable
@@ -239,3 +249,42 @@ fun CheckBoxItem(
             }
 }
 
+@Preview
+@Composable
+fun ProductoItem(
+
+) {
+    var nombre: String = "Camiseta basica"
+    var imagenResId: Int =
+    var propiedad1: String = "precio 23$"
+    var propiedad2: String = "descripcion del producto"
+    Row(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = imagenResId),
+        ontentDescription = null,
+            modifier = Modifier
+                .size(120.dp)
+                .clip(shape = RoundedCornerShape(8.dp)),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        // Detalles del producto
+        Column {
+            Text(
+                text = nombre,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Propiedad 1: $propiedad1", fontSize = 14.sp)
+            Text(text = "Propiedad 2: $propiedad2", fontSize = 14.sp)
+        }
+    }
+}
