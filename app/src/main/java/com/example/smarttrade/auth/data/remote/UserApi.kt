bce3e28f959ef30d4db360b98d6df7ca62261d5d
andresdlt03 <andelatmo@gmail.com>
2024-04-17 02:04:14 +0200

    login finished

--- app/src/main/java/com/example/smarttrade/auth/data/remote/UserApi.kt
+++ app/src/main/java/com/example/smarttrade/auth/data/remote/UserApi.kt
@@ -2,14 +2,18 @@ package com.example.smarttrade.auth.data.remote
 
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface UserApi {
 
+    @Headers("content-type: application/json")
     @POST("users/{userType}s")
     suspend fun registerUser(@Body user: String, @Path("userType") userType: String): Response<String>
 
+
+    @Headers("content-type: application/json")
     @POST("users/login")
     suspend fun loginUser(@Body loginRequest: String): Response<String>
 }
